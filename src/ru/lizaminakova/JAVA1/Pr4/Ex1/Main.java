dd3728489a323577db1bc2659853a9f723edc58d
Elizaveta Minakova <elizaveta.minakova@icloud.com>
2020-09-14 12:03:32 +0300

    14.09.2020

--- src/ru/lizaminakova/JAVA1/Pr4/Ex1/Main.java
+++ src/ru/lizaminakova/JAVA1/Pr4/Ex1/Main.java
@@ -2,6 +2,7 @@ package ru.lizaminakova.JAVA1.Pr4.Ex1;
 
 public class Main {
     public static void main(String[] args) {
+
         Shape s1 = new Circle( "RED", false, 5.5);
 
         System.out.println(s1);
