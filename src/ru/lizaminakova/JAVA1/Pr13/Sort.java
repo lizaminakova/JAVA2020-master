7ba8b16f9719b662e2440ee00a68bcfc7e64d7f6
Elizaveta Minakova <elizaveta.minakova@icloud.com>
2020-11-01 22:16:24 +0300

    1.11.20

---
+++ src/ru/lizaminakova/JAVA1/Pr13/Sort.java
@@ -0,0 +1,10 @@
+package ru.lizaminakova.JAVA1.Pr13;
+
+import java.util.Comparator;
+
+public class Sort implements Comparator<Student> {
+    @Override
+    public int compare(Student o1, Student o2) {
+        return o1.getGpa() < o2.getGpa() ? 1 : -1;
+    }
+}
---
+++ src/ru/lizaminakova/JAVA1/Pr14/Test.java
@@ -0,0 +1,8 @@
+package ru.lizaminakova.JAVA1.Pr14;
+
+public interface Test {
+    double getGpa();
+    int getiDNumber();
+    void setiDNumber(int iDNumber);
+    void setGpa(double gpa);
+}
